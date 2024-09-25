01caaebedca47e3a8a9696cf1972c68563a6f5da
Sarah Herbert <ms.sherbert@gmail.com>
2024-09-25 21:28:30 +1200

    Update Mt+1 and CPUE model code and outputs

--- Code/Capture-time-inundation-analysis.R
+++ Code/Capture-time-inundation-analysis.R
@@ -162,250 +162,60 @@ cor(OP[,c(3,7:12)],method="spearman") #check for covariance among potential fixe
 #No warning messages from any of the following eight models
 #fit is singular if spatial random component is Site/Grid
 
-M1.cOP <- lmer(log(CPUE+0.01) ~ Season.centred*BA*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = OP)
-M2.cOP <- lmer(log(CPUE+0.01) ~ Season.centred*BA*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = OP)
-M3.cOP <- lmer(log(CPUE+0.01) ~ Season.centred*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = OP)
-M4.cOP <- lmer(log(CPUE+0.01) ~ Season.centred*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = OP)
 
-models.cOP <- list(M1.cOP,M2.cOP,M3.cOP,M4.cOP)
-Avg.models.cOP <- modelAvg(models.cOP, opt = TRUE)
+M.cOP <- lmer(log(CPUE+0.01) ~ BA*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = OP)
+summary(M.cOP) #view summary of model
 
-cAIC.M1.cOP <- cAIC(M1.cOP)
-cAIC.M2.cOP <- cAIC(M2.cOP)
-cAIC.M3.cOP <- cAIC(M3.cOP)
-cAIC.M4.cOP <- cAIC(M4.cOP)
+write.csv(coef(summary(M.cOP)), "Outputs/FixedEff_M_cOP.csv")
 
-AIC.cOP <- unlist(c(cAIC.M1.cOP[5],cAIC.M2.cOP[5],cAIC.M3.cOP[5],cAIC.M4.cOP[5]))
-df.cOP <- unlist(c(cAIC.M1.cOP[2],cAIC.M2.cOP[2],cAIC.M3.cOP[2],cAIC.M4.cOP[2]))
-DAIC.cOP <- Delta.AIC(AIC.cOP)
-
-Cand.model.cOP <- as.data.frame(cbind(AIC.cOP, DAIC.cOP, df.cOP))
-Model.selection.cOP <- Cand.model.cOP %>%
-  reframe("Model" = c(1:4),
-          "cAIC" = round(AIC.cOP,2), 
-          "DcAIC" = round(DAIC.cOP,2),
-          "Estimated DF" = round(df.cOP,2)
-  ) %>%
-  arrange(cAIC)
-
-Model.selection.cOP
-
-summary(M1.cOP) #view summary of model with lowest AIC
-
-simulationOutput.M1.cOP <- simulateResiduals(fittedModel = M1.cOP, plot = F) #assess fit of model with lowest AIC
-plotQQunif(simulationOutput.M1.cOP) # left plot in plot.DHARMa()
-plotResiduals(simulationOutput.M1.cOP) # right plot in plot.DHARMa()
-
-write.csv(coef(summary(M1.cOP)), "Outputs/FixedEff_M1_cOP.csv")
-write.csv(Model.selection.cOP,"Outputs/ModSel_cOP.csv")
+simulationOutput.M.cOP <- simulateResiduals(fittedModel = M.cOP, plot = F) #assess fit of model
+plotQQunif(simulationOutput.M.cOP) # left plot in plot.DHARMa()
+plotResiduals(simulationOutput.M.cOP) # right plot in plot.DHARMa()
 
 #--- Woodworthia maculata CPUE ---#
 
 #No warning messages produced by any of these models
-M1.cWM <- lmer(log(CPUE+0.01) ~ Season.centred*BA*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = WM)
-M2.cWM <- lmer(log(CPUE+0.01) ~ Season.centred*BA*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = WM)
-M3.cWM <- lmer(log(CPUE+0.01) ~ Season.centred*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = WM)
-M4.cWM <- lmer(log(CPUE+0.01) ~ Season.centred*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = WM)
+M.cWM <- lmer(log(CPUE+0.01) ~ BA*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = WM)
+summary(M.cWM) #view summary of model
 
-models.cWM <- list(M1.cWM,M2.cWM,M3.cWM,M4.cWM)
-Avg.models.cWM <- modelAvg(models.cWM, opt = TRUE)
+simulationOutput.M.cWM <- simulateResiduals(fittedModel = M.cWM, plot = F) #assess fit of model with lowest AIC
+plotQQunif(simulationOutput.M.cWM) # left plot in plot.DHARMa()
+plotResiduals(simulationOutput.M.cWM) # right plot in plot.DHARMa()
 
-cAIC.M1.cWM <- cAIC(M1.cWM)
-cAIC.M2.cWM <- cAIC(M2.cWM)
-cAIC.M3.cWM <- cAIC(M3.cWM)
-cAIC.M4.cWM <- cAIC(M4.cWM)
+write.csv(coef(summary(M.cWM)), "Outputs/FixedEff_M_cWM.csv")
 
-AIC.cWM <- unlist(c(cAIC.M1.cWM[5],cAIC.M2.cWM[5],cAIC.M3.cWM[5],cAIC.M4.cWM[5]))
-df.cWM <- unlist(c(cAIC.M1.cWM[2],cAIC.M2.cWM[2],cAIC.M3.cWM[2],cAIC.M4.cWM[2]))
-DAIC.cWM <- Delta.AIC(AIC.cOP)
-
-Cand.model.cWM <- as.data.frame(cbind(AIC.cWM, DAIC.cWM, df.cWM))
-Model.selection.cWM <- Cand.model.cWM %>%
-  reframe("Model" = c(1:4),
-          "cAIC" = round(AIC.cWM,2), 
-          "DcAIC" = round(DAIC.cWM,2),
-          "Estimated DF" = round(df.cWM,2)
-  ) %>%
-  arrange(cAIC)
-
-Model.selection.cWM
-
-summary(M3.cWM) #view summary of model with lowest AIC
-
-simulationOutput.M3.cWM <- simulateResiduals(fittedModel = M3.cWM, plot = F) #assess fit of model with lowest AIC
-plotQQunif(simulationOutput.M3.cWM) # left plot in plot.DHARMa()
-plotResiduals(simulationOutput.M3.cWM) # right plot in plot.DHARMa()
-
-write.csv(coef(summary(M3.cWM)), "Outputs/FixedEff_M3_cWM.csv")
-write.csv(Model.selection.cWM,"Outputs/ModSel_cWM.csv")
-
-#--- All lizards CPUE ---#
-
-#No warning messages produced by these models
-M1.cALL <- lmer(sqrt(CPUE+0.01) ~ Season.centred*BA*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = DF) #fit is singular if spatial random component is Site/Grid
-M2.cALL <- lmer(sqrt(CPUE+0.01) ~ Season.centred*BA*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = DF)
-M3.cALL <- lmer(sqrt(CPUE+0.01) ~ Season.centred*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = DF) 
-M4.cALL <- lmer(sqrt(CPUE+0.01) ~ Season.centred*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = DF)
-
-models.cALL <- list(M1.cALL,M2.cALL,M3.cALL,M4.cALL)
-Avg.models.cALL <- modelAvg(models.cALL, opt = TRUE)
-
-cAIC.M1.cALL <- cAIC(M1.cALL)
-cAIC.M2.cALL <- cAIC(M2.cALL)
-cAIC.M3.cALL <- cAIC(M3.cALL)
-cAIC.M4.cALL <- cAIC(M4.cALL)
-
-AIC.cALL <- unlist(c(cAIC.M1.cALL[5],cAIC.M2.cALL[5],cAIC.M3.cALL[5],cAIC.M4.cALL[5]))
-df.cALL <- unlist(c(cAIC.M1.cALL[2],cAIC.M2.cALL[2],cAIC.M3.cALL[2],cAIC.M4.cALL[2]))
-DAIC.cALL <- Delta.AIC(AIC.cALL)
-
-Cand.model.cALL <- as.data.frame(cbind(AIC.cALL, DAIC.cALL, df.cALL))
-Model.selection.cALL <- Cand.model.cALL %>%
-  reframe("Model" = c(1:4),
-          "cAIC" = round(AIC.cALL,2), 
-          "DcAIC" = round(DAIC.cALL,2),
-          "Estimated DF" = round(df.cALL,2)
-  ) %>%
-  arrange(cAIC)
-
-Model.selection.cALL
-
-summary(M3.cALL) #view summary of model with lowest AIC
-
-simulationOutput.M3.cALL <- simulateResiduals(fittedModel = M3.cALL, plot = F) #assess fit of model with lowest AIC
-plotQQunif(simulationOutput.M3.cALL) # left plot in plot.DHARMa()
-plotResiduals(simulationOutput.M3.cALL) # right plot in plot.DHARMa()
-
-write.csv(coef(summary(M3.cALL)), "Outputs/FixedEff_M3_cALL.csv")
-write.csv(Model.selection.cALL,"Outputs/ModSel_cALL.csv")
 
 #--- Oligosoma polychroma Mt+1 ---#
 
-M1.mOP <- lmer(log(Mt1+0.01) ~ Season.centred*BA*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = OP) #singular
-M2.mOP <- lmer(log(Mt1+0.01) ~ Season.centred*BA*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = OP)
-M3.mOP <- lmer(log(Mt1+0.01) ~ Season.centred*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = OP) #singular
-M4.mOP <- lmer(log(Mt1+0.01) ~ Season.centred*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = OP) #singular
-M5.mOP <- lmer(log(Mt1+0.01) ~ Season.centred*BA*Inundated + T.min + Pit.repl + (1|Grid), data = OP)
-M6.mOP <- lmer(log(Mt1+0.01) ~ Season.centred*BA*Inundated + T.max + Pit.repl + (1|Grid), data = OP)
-M7.mOP <- lmer(log(Mt1+0.01) ~ Season.centred*Inundated + T.min + Pit.repl + (1|Grid), data = OP)
-M8.mOP <- lmer(log(Mt1+0.01) ~ Season.centred*Inundated + T.max + Pit.repl + (1|Grid), data = OP)
+M1.mOP <- lmer(log(Mt1+0.01) ~ BA*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = OP)
+M2.mOP <- lmer(log(Mt1+0.01) ~ BA*Inundated + T.min + Pit.repl + (1|Grid), data = OP)
+
 
 #Test whether simplifying random term creates a significant difference 
 #in any pair of models with same fixed factor structure
-anova(M1.mOP,M5.mOP) #p = 0.9854
-anova(M2.mOP,M6.mOP) #p = 0.9943
-anova(M3.mOP,M7.mOP) #p = 0.9989
-anova(M4.mOP,M8.mOP) #p = 0.9998
-#No, so decided to use models with random intercept term (1|Grid) in candidate set
-models.mOP <- list(M5.mOP,M6.mOP,M7.mOP,M8.mOP)
-Avg.models.mOP <- modelAvg(models.mOP, opt = TRUE)
+anova(M1.mOP,M2.mOP) #p = 1
 
-cAIC.M5.mOP <- cAIC(M5.mOP)
-cAIC.M6.mOP <- cAIC(M6.mOP)
-cAIC.M7.mOP <- cAIC(M7.mOP)
-cAIC.M8.mOP <- cAIC(M8.mOP)
+#No, so decided to use models with random intercept term (1|Grid)\
 
-AIC.mOP <- unlist(c(cAIC.M5.mOP[5],cAIC.M6.mOP[5],cAIC.M7.mOP[5],cAIC.M8.mOP[5]))
-df.mOP <- unlist(c(cAIC.M5.mOP[2],cAIC.M6.mOP[2],cAIC.M7.mOP[2],cAIC.M8.mOP[2]))
-DAIC.mOP <- Delta.AIC(AIC.mOP)
+summary(M2.mOP) #view summary of model
 
-Cand.model.mOP <- as.data.frame(cbind(AIC.mOP, DAIC.mOP, df.mOP))
-Model.selection.mOP <- Cand.model.mOP %>%
-  reframe("Model" = c(1:4),
-          "cAIC" = round(AIC.mOP,2), 
-          "DcAIC" = round(DAIC.mOP,2),
-          "Estimated DF" = round(df.mOP,2)
-  ) %>%
-  arrange(cAIC)
+simulationOutput.M2.mOP <- simulateResiduals(fittedModel = M2.mOP, plot = F) #assess fit of model with lowest AIC
+plotQQunif(simulationOutput.M2.mOP) # left plot in plot.DHARMa()
+plotResiduals(simulationOutput.M2.mOP) # right plot in plot.DHARMa()
 
-Model.selection.mOP
+write.csv(coef(summary(M2.mOP)), "Outputs/FixedEff_M2_mOP.csv")
 
-summary(M7.mOP) #view summary of model with lowest AIC (in this set model 3 is model M7)
-
-simulationOutput.M7.mOP <- simulateResiduals(fittedModel = M7.mOP, plot = F) #assess fit of model with lowest AIC
-plotQQunif(simulationOutput.M7.mOP) # left plot in plot.DHARMa()
-plotResiduals(simulationOutput.M7.mOP) # right plot in plot.DHARMa()
-
-write.csv(coef(summary(M7.mOP)), "Outputs/FixedEff_M7_mOP.csv")
-write.csv(Model.selection.mOP,"Outputs/ModSel_mOP.csv")
 
 #--- Woodworthia maculata Mt+1 ---#
 
-M1.mWM <- lmer(log(Mt1+0.01) ~ Season.centred*BA*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = WM)
-M2.mWM <- lmer(log(Mt1+0.01) ~ Season.centred*BA*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = WM)
-M3.mWM <- lmer(log(Mt1+0.01) ~ Season.centred*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = WM)
-M4.mWM <- lmer(log(Mt1+0.01) ~ Season.centred*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = WM)
+M.mWM <- lmer(log(Mt1+0.01) ~ BA*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = WM)
+summary(M.mWM) #view summary of model
 
-models.mWM <- list(M1.mWM,M2.mWM,M3.mWM,M4.mWM)
-Avg.models.mWM <- modelAvg(models.mWM, opt = TRUE)
+simulationOutput.M.mWM <- simulateResiduals(fittedModel = M.mWM, plot = F) #assess fit of model with lowest AIC
+plotQQunif(simulationOutput.M.mWM) # left plot in plot.DHARMa()
+plotResiduals(simulationOutput.M.mWM) # right plot in plot.DHARMa()
 
-cAIC.M1.mWM <- cAIC(M1.mWM)
-cAIC.M2.mWM <- cAIC(M2.mWM)
-cAIC.M3.mWM <- cAIC(M3.mWM)
-cAIC.M4.mWM <- cAIC(M4.mWM)
-
-AIC.mWM <- unlist(c(cAIC.M1.mWM[5],cAIC.M2.mWM[5],cAIC.M3.mWM[5],cAIC.M4.mWM[5]))
-df.mWM <- unlist(c(cAIC.M1.mWM[2],cAIC.M2.mWM[2],cAIC.M3.mWM[2],cAIC.M4.mWM[2]))
-DAIC.mWM <- Delta.AIC(AIC.mWM)
-
-Cand.model.mWM <- as.data.frame(cbind(AIC.mWM, DAIC.mWM, df.mWM))
-Model.selection.mWM <- Cand.model.mWM %>%
-  reframe("Model" = c(1:4),
-          "cAIC" = round(AIC.mWM,2), 
-          "DcAIC" = round(DAIC.mWM,2),
-          "Estimated DF" = round(df.mWM,2)
-  ) %>%
-  arrange(cAIC)
-
-Model.selection.mWM
-
-summary(M1.mWM) #view summary of model with lowest AIC
-
-simulationOutput.M1.mWM <- simulateResiduals(fittedModel = M1.mWM, plot = F) #assess fit of model with lowest AIC
-plotQQunif(simulationOutput.M1.mWM) # left plot in plot.DHARMa()
-plotResiduals(simulationOutput.M1.mWM) # right plot in plot.DHARMa()
-
-write.csv(coef(summary(M1.mWM)), "Outputs/FixedEff_M1_mWM.csv")
-write.csv(Model.selection.mWM,"Outputs/ModSel_mWM.csv")
-
-#--- All lizards Mt+1 ---#
-
-M1.mALL <- lmer(sqrt(Mt1+0.01) ~ Season.centred*BA*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = DF)
-M2.mALL <- lmer(sqrt(Mt1+0.01) ~ Season.centred*BA*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = DF)
-M3.mALL <- lmer(sqrt(Mt1+0.01) ~ Season.centred*Inundated + T.min + Pit.repl + (Season.centred|Grid), data = DF)
-M4.mALL <- lmer(sqrt(Mt1+0.01) ~ Season.centred*Inundated + T.max + Pit.repl + (Season.centred|Grid), data = DF)
-
-models.mALL <- list(M1.mALL,M2.mALL,M3.mALL,M4.mALL)
-Avg.models.mALL <- modelAvg(models.mALL, opt = TRUE)
-
-cAIC.M1.mALL <- cAIC(M1.mALL)
-cAIC.M2.mALL <- cAIC(M2.mALL)
-cAIC.M3.mALL <- cAIC(M3.mALL)
-cAIC.M4.mALL <- cAIC(M4.mALL)
-
-AIC.mALL <- unlist(c(cAIC.M1.mALL[5],cAIC.M2.mALL[5],cAIC.M3.mALL[5],cAIC.M4.mALL[5]))
-df.mALL <- unlist(c(cAIC.M1.mALL[2],cAIC.M2.mALL[2],cAIC.M3.mALL[2],cAIC.M4.mALL[2]))
-DAIC.mALL <- Delta.AIC(AIC.mALL)
-
-Cand.model.mALL <- as.data.frame(cbind(AIC.mALL, DAIC.mALL, df.mALL))
-Model.selection.mALL <- Cand.model.mALL %>%
-  reframe("Model" = c(1:4),
-          "cAIC" = round(AIC.mALL,2), 
-          "DcAIC" = round(DAIC.mALL,2),
-          "Estimated DF" = round(df.mALL,2)
-  ) %>%
-  arrange(cAIC)
-
-Model.selection.mALL
-
-summary(M4.mALL) #view summary of model with lowest AIC
-
-simulationOutput.M4.mALL <- simulateResiduals(fittedModel = M4.mALL, plot = F) #assess fit of model with lowest AIC
-plotQQunif(simulationOutput.M4.mALL) # left plot in plot.DHARMa()
-plotResiduals(simulationOutput.M4.mALL) # right plot in plot.DHARMa()
-
-write.csv(coef(summary(M4.mALL)), "Outputs/FixedEff_M4_mALL.csv")
-write.csv(Model.selection.mALL,"Outputs/ModSel_mALL.csv")
+write.csv(coef(summary(M.mWM)), "Outputs/FixedEff_M_mWM.csv")
 
 #=== Plot best model outcomes ===#
 
@@ -470,33 +280,6 @@ WMplot <-
   theme_bw() +
   theme(plot.title = element_text(face = "italic"),legend.title=element_blank())
 
-#All
-
-DF$labelsDF <- rep("Inundated",times=length(DF$CPUE))
-DF$labelsDF[DF$Inundated == 0] <- "Not affected"
-DF$Labels <- paste(DF$Grid,sep=": ",DF$labelsDF)
-
-DF <- DF %>% 
-  mutate(fit.m = predict(M3.cALL, re.form = NA), # marginal fits (i.e. fixed effects only)
-         fit.c = predict(M3.cALL, re.form = NULL)) #conditional fits (i.e. fixed + random effects)
-
-DFplot <- 
-  ggplot(DF, aes(y = sqrt(Mt1+0.01),x=Season,color=labelsDF)) +
-  geom_smooth(data = DF,aes(y = fit.m),method="lm",se = FALSE) + #no BA term
-  geom_point() + 
-  #geom_point(data = subset(DF,Season < 2020.915), aes(y = fit.m),pch=3) +
-  #geom_point(data = subset(DF,Season > 2020.915), aes(y = fit.m),pch=3) +
-  scale_colour_manual(values=c('Not affected'="grey70",'Inundated'= "grey40")) +
-  geom_vline(xintercept = 2020.33,color="#89c8f4",linewidth=1) +
-  #facet_wrap(~factor(Labels,
-  #                   levels=c('MP1: Inundated','MP2: Not affected','RE1: Not affected',
-  #                            'WP3: Inundated','WP2: Not affected','RE2: Not affected'))) +
-  #facet_wrap(~factor(labelsDF)) +
-  scale_y_continuous(name="Square root-transformed CPUE") +
-  scale_x_continuous(name="Date") +
-  ggtitle(label = "All lizards") + 
-  theme_bw() +
-  theme(legend.title=element_blank())
 
 #--- Mt+1 ---#
 
@@ -547,30 +330,6 @@ WMplot <-
   theme_bw() +
   theme(plot.title = element_text(face = "italic"),legend.title=element_blank())
 
-#All
-
-DF <- DF %>% 
-  mutate(fit.m = predict(M4.mALL, re.form = NA), # marginal fits (i.e. fixed effects only)
-         fit.c = predict(M4.mALL, re.form = NULL)) #conditional fits (i.e. fixed + random effects)
-
-DFplot <- 
-  ggplot(DF, aes(y = sqrt(Mt1+0.01),x=Season,color=labelsDF)) +
-  #geom_smooth(data = subset(DF,Season < 2020.915),aes(y = fit.m),method="lm",se = FALSE) + 
-  #geom_smooth(data = subset(DF,Season > 2020.915),aes(y = fit.m),method="lm", se = FALSE) +
-  geom_point() + 
-  #geom_point(data = subset(DF,Season < 2020.915), aes(y = fit.m),pch=3) +
-  #geom_point(data = subset(DF,Season > 2020.915), aes(y = fit.m),pch=3) +
-  scale_colour_manual(values=c('Not affected'="grey70",'Inundated'= "grey40")) +
-  geom_vline(xintercept = 2020.33,color="#89c8f4",linewidth=1) +
-  #facet_wrap(~factor(Labels,
-  #                   levels=c('MP1: Inundated','MP2: Not affected','RE1: Not affected',
-  #                            'WP3: Inundated','WP2: Not affected','RE2: Not affected'))) +
-  #facet_wrap(~factor(labelsDF)) +
-  scale_y_continuous(name="Square root-transformed Mt+1 / day") +
-  scale_x_continuous(name="Date") +
-  ggtitle(label = "All lizards") + 
-  theme_bw() +
-  theme(legend.title=element_blank())
 
 #--- Export multiplots ---#
 
